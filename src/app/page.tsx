352179584c835aafb4c895abdb25e199d1de6e92
Ebin746 <146419456+Ebin746@users.noreply.github.com>
2025-01-01 17:53:32 +0530

    feet:implemented Navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,16 +10,6 @@ import {
 import { ModeToggle } from '@/components/ModeToggle'
 export default function Home() {
 return(<div>
- <SignedOut>
-            <SignInButton mode='model'>
-           
-              <Button> Sign in </Button>
-              </SignInButton>
-          </SignedOut>
-          <SignedIn>
-            <UserButton />
-          </SignedIn>
-          <ModeToggle/>
-          <Button variant={"secondary"}>Click me</Button>
+ hi
 </div>)
 }
